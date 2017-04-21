82662fb703fa4a6768ad501c7968b6b2c61ca0d9
Kristina <kristina.kabosiene@gmail.com>
2017-04-21 13:55:18 +0300

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170421071043) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "documents", force: :cascade do |t|
     t.string   "name"
     t.string   "client"
