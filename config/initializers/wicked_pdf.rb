e18a57e7cf337ca4247b6a08673eb0f4af2dccf5
Kristina <kristina.kabosiene@gmail.com>
2017-04-24 16:00:05 +0300

    add changes to init wicked_pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -21,8 +21,6 @@ WickedPdf.config = {
 }
 if Rails.env.production?
   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf"
-else
-  wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"
 end
 
 WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
